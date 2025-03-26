4550abd814446fdd4880617a8a5023d4e9306553
pcoundia <papacoundia@gmail.com>
2025-03-26 14:12:32 +0000

    feat: add find product by name - clean code

--- src/main/java/com/groupe2cs/bizyhub/products/infrastructure/axon/AxonConfig.java
+++
@@ -1,5 +0,0 @@
-package com.groupe2cs.bizyhub.products.infrastructure.axon;
-
-public class AxonConfig {
-
-}
---
+++ src/main/java/com/groupe2cs/bizyhub/shared/infrastructure/axon/AxonConfig.java
@@ -0,0 +1,5 @@
+package com.groupe2cs.bizyhub.shared.infrastructure.axon;
+
+public class AxonConfig {
+
+}
